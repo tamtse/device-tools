b13f4c2a70df75fdb3165f1e57f16d0b0852434e
feugana1g <feugana1@gmail.com>
2026-01-23 00:50:27 +0100

    feat: healthcheck, docker-compose ready

--- device-registration-api/app/main.py
+++ device-registration-api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.register import router as register_router
+from app.api.health import router as health_router
 from app.__version__ import __version__
 from app.db.init_db import lifespan
 from app.core.logging import setup_logging
@@ -12,5 +13,5 @@ app = FastAPI(
     version=__version__,
     lifespan=lifespan
 )
-
+app.include_router(health_router)
 app.include_router(register_router)
---
+++ statistics-api/app/api/statistics.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Query
+from app.services.statistics_service import StatisticsService
+
+router = APIRouter(prefix="/Log/auth", tags=["Statistics"])
+
+@router.get("/statistics")
+async def get_statistics(deviceType: str = Query(...)):
+    service = StatisticsService()
+    count = await service.get_device_statistics(deviceType)
+
+    return {
+        "deviceType": deviceType,
+        "count": count
+    }
+
+@router.get("/statistics/devices")
+async def get_all_statistics():
+    service = StatisticsService()
+    return await service.get_all_device_statistics()
+
+@router.get("/health")
+async def get_all_statistics():
+    service = StatisticsService()
+    return await service.get_all_device_statistics()
